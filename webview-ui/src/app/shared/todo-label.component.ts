093d223183a56d1d13128a0602a8c721c7f44913
Francesco Anzalone <francesco.a@mail.com>
2023-10-28 15:55:18 +0100

    [UI] todo-label component:  Add creation and completion date.

--- webview-ui/src/app/shared/todo-label.component.ts
+++ webview-ui/src/app/shared/todo-label.component.ts
@@ -8,16 +8,4 @@ import { Todo } from "../../../../src/todo/store";
 })
 export class TodoLabel {
 	@Input() todo!: Todo;
-	creationDate!: Date;
-
-	constructor() {
-		if (this.todo?.creationDate) {
-			this.creationDate = this.parseDateString(this.todo.creationDate);
-		}
-	}
-
-	// Function to parse date strings into Date objects
-	parseDateString(dateString: string): Date {
-		return new Date(dateString);
-	}
 }
