704d2d8df967788049e18cb6a8f10cd54c1174e4
Francesco Anzalone <francesco.a@mail.com>
2023-11-11 17:13:44 +0000

    [UI] Add styling for panel tabs.

--- webview-ui/src/app/app.component.ts
+++ webview-ui/src/app/app.component.ts
@@ -46,7 +46,7 @@ provideVSCodeDesignSystem().register(
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.css"],
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
 	level: TodoLevel = TodoLevel.workspace;
