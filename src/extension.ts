d7aac332cf77a012a54d5898c4d384b596c0f59c
Francesco Anzalone <francesco.a@mail.com>
2023-12-26 22:41:53 +0000

    Add logic for status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,32 @@
 import { commands, ExtensionContext } from "vscode";
 import { HelloWorldPanel } from "./panels/HelloWorldPanel";
-import { createStatusBarItem } from "./statusBarItem";
-import createStore, { storeActions, persist, Todo, FullData } from "./todo/store";
+import { createStatusBarItem, updateStatusBarItem } from "./statusBarItem";
+import createStore, { storeActions, persist, FullData } from "./todo/store";
 
 export function activate(context: ExtensionContext) {
 	const store = createStore();
+
+	const openTodoCommand = commands.registerCommand("vscode-tasks.openTodo", () => {
+		HelloWorldPanel.render(context, store);
+	});
+
+	// Add command to the extension context
+	context.subscriptions.push(openTodoCommand);
+
+	createStatusBarItem(context);
+
+	store.subscribe(() => {
+		const state: FullData = store.getState();
+		// Update Webview
+		HelloWorldPanel.currentPanel?.update(state);
+
+		// Save data
+		persist(store, context);
+
+		//Update status bar
+		updateStatusBarItem(state);
+	});
+
 	store.dispatch(
 		storeActions.loadData({
 			data: {
@@ -13,31 +35,4 @@ export function activate(context: ExtensionContext) {
 			} as FullData,
 		})
 	);
-
-	console.log("Initial data:");
-	console.log({
-		workspaceTodos: context.workspaceState.get("TodoData") ?? [],
-		userTodos: context.globalState.get("TodoData") ?? [],
-	} as FullData);
-	console.log("Store data:");
-	console.log(store.getState());
-
-	// Create the show hello world command
-	const openTodoCommand = commands.registerCommand("vscode-tasks.openTodo", () => {
-		HelloWorldPanel.render(context, store);
-	});
-
-	// Add command to the extension context
-	context.subscriptions.push(openTodoCommand);
-
-	const statusBarItem = createStatusBarItem(context);
-
-	store.subscribe(() => {
-		console.log("State changed: ");
-		console.log(store.getState());
-		HelloWorldPanel.currentPanel?.update(store.getState());
-		console.log("UI Updated!");
-		persist(store, context);
-		// TODO: update statusBarItem
-	});
 }
--- src/statusBarItem.ts
+++ src/statusBarItem.ts
@@ -1,10 +1,48 @@
 import * as vscode from "vscode";
+import { FullData } from "./todo/store";
 
-export function createStatusBarItem (context: vscode.ExtensionContext) {
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-  statusBarItem.command = "vscode-tasks.openTodo";
-  statusBarItem.text="Tasks";
-  statusBarItem.show();
-  context.subscriptions.push(statusBarItem);
-  return statusBarItem;
-};
+let _statusBarItem: vscode.StatusBarItem | undefined;
+
+/**
+ * Creates a status bar item and adds it to the vscode extension context.
+ *
+ * @param {vscode.ExtensionContext} context - The vscode extension context.
+ * @return {void} There is no return value.
+ */
+export function createStatusBarItem(context: vscode.ExtensionContext) {
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = "vscode-tasks.openTodo";
+	statusBarItem.text = "Todo";
+	statusBarItem.tooltip = "Open Todos";
+	statusBarItem.show();
+	context.subscriptions.push(statusBarItem);
+	_statusBarItem = statusBarItem;
+}
+
+/**
+ * Updates the status bar item with the current number of todos in the workspace and for the user.
+ *
+ * @param {string} workspaceTodos - The number of todos in the workspace.
+ * @param {string} userTodos - The number of todos for the user.
+ */
+export function updateStatusBarItem(state: FullData) {
+	if (!_statusBarItem) return;
+
+	const { workspace, user } = getNumberOfTodos(state);
+	_statusBarItem.text = `Todo ${workspace} W / ${user} U`;
+	_statusBarItem.tooltip = new vscode.MarkdownString(
+		`Open Todos\n- Workspace: ${workspace}\n- User: ${user}`
+	);
+}
+
+/**
+ * Calculate the number of incomplete todos in the given state.
+ *
+ * @param {FullData} state - The state containing the todos.
+ * @return {{workspace: number, user: number}} - An object containing the number of todos in the workspace and for the user.
+ */
+function getNumberOfTodos(state: FullData) {
+	const workspace = state.workspaceTodos?.filter((todo) => !todo.completed).length ?? 0;
+	const user = state.userTodos?.filter((todo) => !todo.completed).length ?? 0;
+	return { workspace, user };
+}
