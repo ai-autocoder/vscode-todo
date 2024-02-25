7623da9d5c53a3526c406bad002601adabf7ba1c
Francesco Anzalone <francesco.a@mail.com>
2024-02-25 14:32:00 +0000

    [UI] Add text-area option autoResize and enable it in new-todo component.

--- webview-ui/src/app/shared/text-area.component.ts
+++ webview-ui/src/app/shared/text-area.component.ts
@@ -1,34 +1,50 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+	AfterViewInit,
+	Component,
+	ElementRef,
+	EventEmitter,
+	Input,
+	OnChanges,
+	Output,
+	QueryList,
+	SimpleChanges,
+	ViewChildren,
+} from "@angular/core";
 
 @Component({
 	selector: "text-area",
 	template: `
 		<vscode-text-area
+			#autoResizeTextArea
 			(input)="onInput($event)"
 			[placeholder]="placeholder"
 			[value]="text"
-			[resize]="'vertical'"
+			[resize]="autoResize ? 'none' : 'vertical'"
 		></vscode-text-area>
 	`,
 	styles: [
 		`
 			vscode-text-area {
 				width: inherit;
+				display: flex;
 			}
 		`,
 		`
 			vscode-text-area::part(control) {
 				font-size: var(--vscode-editor-font-size);
+				max-height: 80vh;
 			}
 		`,
 	],
 })
-export class TextArea {
+export class TextArea implements AfterViewInit, OnChanges {
 	text: string = "";
+	currentHeight: number = 0;
 
 	@Output() valueChange = new EventEmitter<string>();
 	@Input() placeholder: string = "";
-
+	@Input() autoResize: boolean = false;
+	@ViewChildren("autoResizeTextArea", { read: ElementRef }) textareaParts!: QueryList<ElementRef>;
 	@Input()
 	get value(): string {
 		return this.text;
@@ -39,7 +55,51 @@ export class TextArea {
 		this.valueChange.emit(this.text);
 	}
 
+	ngAfterViewInit(): void {
+		this.adjustHeight();
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes["autoResize"] || changes["value"]) {
+			this.adjustHeight();
+		}
+	}
+
 	onInput(event: Event) {
 		this.value = (event.target as HTMLTextAreaElement).value;
+	}
+
+	adjustHeight(): void {
+		setTimeout(() => {
+			const extraSpace = 3;
+			this.textareaParts.forEach((part) => {
+				const textarea: HTMLTextAreaElement =
+					part.nativeElement.shadowRoot?.querySelector('[part="control"]');
+
+				// If autoResize is true, set overflow to hidden to avoid scrollbars to appear before the height is calculated.
+				textarea.style.overflow = this.autoResize ? "hidden" : "auto";
+				// Reset the height to get accurate scrollHeight
+				textarea.style.height = "auto";
+				if (!this.autoResize) return;
+
+				requestAnimationFrame(() => {
+					let newHeight = textarea.scrollHeight + extraSpace;
+					const scrollHeightDiff = Math.abs(newHeight - this.currentHeight);
+
+					if (this.currentHeight != 0 && scrollHeightDiff <= extraSpace) {
+						newHeight = this.currentHeight;
+					}
+
+					textarea.style.height = `${newHeight}px`;
+					this.currentHeight = newHeight;
+
+					// If maxHeight is exceeded, set overflow to auto
+					const maxHeight = parseInt(window.getComputedStyle(textarea).maxHeight, 10);
+					if (newHeight > maxHeight) {
+						textarea.style.overflow = "auto";
+					}
+				});
+			});
+		}, 0);
 	}
 }
