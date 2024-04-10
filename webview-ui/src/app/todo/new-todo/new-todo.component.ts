253b1a6ebe86ea580f8f869012fbf626bf8f7ed5
Francesco Anzalone <francesco.a@mail.com>
2024-04-10 14:07:16 +0100

    [UI] File tab: Disable add button when no file is set to prevent adding new todos without proper context.

--- webview-ui/src/app/todo/new-todo/new-todo.component.ts
+++ webview-ui/src/app/todo/new-todo/new-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { TodoScope } from "../../../../../src/todo/todoTypes";
 import { TodoService } from "../todo.service";
 
@@ -7,10 +7,19 @@ import { TodoService } from "../todo.service";
 	templateUrl: "./new-todo.component.html",
 	styleUrls: ["./new-todo.component.css"],
 })
-export class NewTodoComponent {
+export class NewTodoComponent implements OnChanges {
 	newTodoText: string = "";
 	@Input() scope!: TodoScope;
+	@Input() currentFilePath!: string;
+	isCurrentFileEmpty = false;
+
 	constructor(private todoService: TodoService) {}
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes["currentFilePath"] || changes["scope"]) {
+			this.isCurrentFileEmpty = this.currentFilePath === "" && this.scope === TodoScope.currentFile;
+		}
+	}
 
 	addTodo($event: Event) {
 		$event.preventDefault();
